7f9e5e08ec303a7fa62cb02ef9d8c578ca72ba6d
Allison Ricker <alliricker@gmail.com>
2020-09-09 18:30:35 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
- puts"#{board[0]}" puts"|" puts"#{board[1]}" puts"|" puts"#{board[2]}"
+ puts"#{board[0]} | #{board[1]} | {board[2]}"
  puts "-----------"
- puts"#{board[3]}" puts"|" puts"#{board[4]}" puts"|" puts"#{board[5]}"
+ puts"#{board[3]} | #{board[4]} | {board[5]}"
  puts "-----------"
- puts"#{board[6]}" puts"|" puts"#{board[7]}" puts"|" puts"#{board[8]}"
+ puts"#{board[6]} | #{board[7]} | #{board[8]}"
  end
 
 
